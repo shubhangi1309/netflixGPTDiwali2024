68c6c3c97105ec208c8169b6072f5394a32ef4df
shubhangi139 <modishubhangi947@gmail.com>
2024-11-02 00:54:26 -0800

    TMDB aPI setuP

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,4 +4,14 @@ export const BACKGROUND = "https://assets.nflxext.com/ffe/siteui/vlv3/151f3e1e-b
 
 export const USER_AVATAR = "https://occ-0-6431-3647.1.nflxso.net/dnm/api/v6/vN7bi_My87NPKvsBoib006Llxzg/AAAABeuqjuQsRgqEDlibtJTI5BMf8IxhLlLOeIT6xI4TL57mqv7XHja43gx02S8pZVe8JNGRQXjnrUk1VcsTXqi83tFKPI6OR3k.png?r=bd7";
 
-export const USER_PHOTO= "https://img.freepik.com/free-photo/view-3d-smiley-happy-sun-with-sky-background_23-2150937820.jpg?t=st=1730468563~exp=1730472163~hmac=653067bc184d8b81307b6466f7913058330b5ffeaa155867cf9786c7b4776510&w=1800";
+export const USER_PHOTO = "https://img.freepik.com/free-photo/view-3d-smiley-happy-sun-with-sky-background_23-2150937820.jpg?t=st=1730468563~exp=1730472163~hmac=653067bc184d8b81307b6466f7913058330b5ffeaa155867cf9786c7b4776510&w=1800";
+
+export const API_OPTIONS = { 
+    method: 'GET', 
+    headers: { 
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmOTAxMDc4YjJhMTVhMDI5N2MyNjA4NTRhMzE2NjFlMCIsIm5iZiI6MTczMDQ4ODE3My43NDQ4NDk3LCJzdWIiOiI2NzI1MjYxMzM5ODVmM2Y0MWFkMWQ4MmMiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.m2J_J-_vvP8pHHIx_IOk50T9lOfceQIjULEpRxNKRKY'
+    } };
+
+    
+    export const API_OPTIONSs = 'https://api.themoviedb.org/3/movie/now_playing'
